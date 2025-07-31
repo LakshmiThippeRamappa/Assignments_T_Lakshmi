a4d8dcf88837f45e0d0ae9f7c55f7f3167ebd7e9
LakshmiThipperamappa <tlakshmi0987@gmail.com>
2025-07-31 18:06:23 +0530

    Fourth commit

---
+++ Spring Assignments/Healthcare/src/main/java/com/main/MainApp.java
@@ -0,0 +1,16 @@
+package com.main;
+
+import com.service.AppointmentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        AppointmentService appointmentService = (AppointmentService) context.getBean("appointmentService");
+
+        String result = appointmentService.book("DOC101", "2025-04-10");
+        System.out.println(result); // "Appointment confirmed" or "Doctor not available"
+    }
+}
